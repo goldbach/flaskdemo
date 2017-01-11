02740e2c9ffa3d439e9709df95b2eff0746ef619
goldbach <jakob.goldbach@gmail.com>
2017-01-11 13:56:52 +0100

    komma spasser

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,7 @@ application = flask.Flask(__name__)
 
 @application.route('/')
 def index():
-  return "Hello World - served from %s!" % socket.gethostname()
+  return "Hello World, spasser  - served from %s!" % socket.gethostname()
   
 if __name__ == "__main__":
     application.run()
